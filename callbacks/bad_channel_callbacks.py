b9c7ca1765e92724a5401f883c075c242f959d50
RobinWeiler <r.weiler@student.vu.nl>
2023-10-11 10:12:19 +0200

    selecting bad channels updates hide-button (bug: leaves out newest bad channel on first click)

--- callbacks/bad_channel_callbacks.py
+++ callbacks/bad_channel_callbacks.py
@@ -42,21 +42,6 @@ def register_bad_channel_callbacks(app):
             bad_channels = globals.raw.info['bads']
 
             return bad_channels, dropdown_channel_names, dropdown_channel_names
-        # elif 'clickData' in trigger:
-        #     # print('Clicked point: {}'.format(clickData))
-        
-        #     channel_index = clickData['points'][0]['curveNumber']
-        #     if channel_index >= len(globals.plotting_data['EEG']['channel_names']):
-        #         return current_selected_bad_channels, current_available_channels, current_available_channels
-
-        #     channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
-            
-        #     if channel_name not in current_selected_bad_channels:
-        #         current_selected_bad_channels.append(channel_name)
-        #     else:
-        #         current_selected_bad_channels.remove(channel_name)
-            
-        #     return current_selected_bad_channels, current_available_channels, current_available_channels
         else:
             return [], [], []
 
@@ -84,70 +69,73 @@ def register_bad_channel_callbacks(app):
             channel_index = clickData['points'][0]['curveNumber']
             if channel_index < len(globals.plotting_data['EEG']['channel_names']):
                 channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
+
+                patched_fig = Patch()
                 
                 if channel_name not in current_selected_bad_channels:
                     current_selected_bad_channels.append(channel_name)
 
-                    patched_fig = Patch()
                     patched_fig['data'][channel_index]['marker']['color'] = c.BAD_CHANNEL_COLOR
                 else:
                     current_selected_bad_channels.remove(channel_name)
 
-                    patched_fig = Patch()
                     patched_fig['data'][channel_index]['marker']['color'] = 'black'
 
                 return patched_fig, current_selected_bad_channels
         else:
             raise PreventUpdate
 
-    # # Update plot when bad channels changed
-    # @app.callback(
-    #     Output('EEG-graph', 'figure', allow_duplicate=True),
-    #     Input('bad-channels-dropdown', 'value'),
-    #     State('EEG-graph', 'figure'),
-    #     prevent_initial_call=True
-    # )
-    # def _update_bad_channels_plot(current_selected_bad_channels, current_fig):
-    #     """Updates plot when bad channels changed.
+    # Update plot when bad channels changed
+    @app.callback(
+        Output('EEG-graph', 'figure', allow_duplicate=True),
+        Input('bad-channels-dropdown', 'value'),
+        State('EEG-graph', 'figure'),
+        prevent_initial_call=True
+    )
+    def _update_bad_channels_plot(current_selected_bad_channels, current_fig):
+        """Updates plot when bad channels changed.
 
-    #     Args:
-    #         current_selected_bad_channels (list): List containing names of currently selected bad channels.
-    #         current_fig (plotly.graph_objs.Figure): The current EEG plot.
+        Args:
+            current_selected_bad_channels (list): List containing names of currently selected bad channels.
+            current_fig (plotly.graph_objs.Figure): The current EEG plot.
 
-    #     Returns:
-    #         tuple(plotly.graph_objs.Figure, int): Updated EEG plot.
-    #     """
-    #     print('trigger')
+        Returns:
+            tuple(plotly.graph_objs.Figure, int): Updated EEG plot.
+        """
+        print('trigger')
 
-    #     if globals.plotting_data:
-    #         globals.raw.info['bads'] = current_selected_bad_channels
-    #         # print(current_selected_bad_channels)
+        if globals.plotting_data:
+            globals.raw.info['bads'] = current_selected_bad_channels
 
-    #         for channel_index in range(len(globals.plotting_data['EEG']['channel_names'])):
-    #             channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
+            patched_fig = Patch()
+
+            for channel_index in range(len(globals.plotting_data['EEG']['channel_names'])):
+                channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
                 
-    #             if channel_name in current_selected_bad_channels:
-    #                 globals.plotting_data['EEG']['default_channel_colors'][channel_index] = c.BAD_CHANNEL_COLOR
-    #                 globals.plotting_data['EEG']['channel_visibility'][channel_index] = False
-    #                 globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = c.BAD_CHANNEL_COLOR
-    #             else:
-    #                 globals.plotting_data['EEG']['default_channel_colors'][channel_index] = 'black'
-    #                 globals.plotting_data['EEG']['channel_visibility'][channel_index] = True
-    #                 globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = 'black'
+                if channel_name in current_selected_bad_channels:
+                    globals.plotting_data['EEG']['default_channel_colors'][channel_index] = c.BAD_CHANNEL_COLOR
+                    globals.plotting_data['EEG']['channel_visibility'][channel_index] = False
+                    globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = c.BAD_CHANNEL_COLOR
+                else:
+                    globals.plotting_data['EEG']['default_channel_colors'][channel_index] = 'black'
+                    globals.plotting_data['EEG']['channel_visibility'][channel_index] = True
+                    globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = 'black'
 
-    #             current_fig['data'][channel_index]['marker']['color'] = globals.plotting_data['EEG']['default_channel_colors'][channel_index]
+                # current_fig['data'][channel_index]['marker']['color'] = globals.plotting_data['EEG']['default_channel_colors'][channel_index]
 
-    #         for model_index in range(len(globals.plotting_data['model'])):
-    #             if globals.plotting_data['model'][model_index]['model_channels']:
-    #                 for channel_index in range(len(globals.plotting_data['EEG']['channel_names'])):
-    #                     channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
-    #                     if channel_name in globals.plotting_data['model'][model_index]['model_channels']:
-    #                         globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = 'blue'
+            for model_index in range(len(globals.plotting_data['model'])):
+                if globals.plotting_data['model'][model_index]['model_channels']:
+                    for channel_index in range(len(globals.plotting_data['EEG']['channel_names'])):
+                        channel_name = globals.plotting_data['EEG']['channel_names'][channel_index]
+                        if channel_name in globals.plotting_data['model'][model_index]['model_channels']:
+                            globals.plotting_data['EEG']['highlighted_channel_colors'][channel_index] = 'blue'
 
-    #         # current_fig['layout']['updatemenus'][0]['buttons'][2]['args2'][0]['visible'] = True
-    #         current_fig['layout']['updatemenus'][0]['buttons'][2]['args'][0]['visible'] = globals.plotting_data['EEG']['channel_visibility']
+            # patched_fig['layout']['updatemenus'][0]['buttons'][2]['args2'][0]['visible'] = True
+            patched_fig['layout']['updatemenus'][0]['buttons'][2]['args'][0]['visible'] = globals.plotting_data['EEG']['channel_visibility']
 
-    #         # current_fig['layout']['updatemenus'][0]['buttons'][3]['args'][0]['marker.color'] = globals.plotting_data['EEG']['highlighted_channel_colors']
-    #         # current_fig['layout']['updatemenus'][0]['buttons'][3]['args2'][0]['marker.color'] = globals.plotting_data['EEG']['default_channel_colors']
+            # current_fig['layout']['updatemenus'][0]['buttons'][3]['args'][0]['marker.color'] = globals.plotting_data['EEG']['highlighted_channel_colors']
+            # current_fig['layout']['updatemenus'][0]['buttons'][3]['args2'][0]['marker.color'] = globals.plotting_data['EEG']['default_channel_colors']
 
-    #     return current_fig
+            return patched_fig
+        else:
+            raise PreventUpdate
