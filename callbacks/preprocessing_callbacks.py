960ea2eedca30b94e33bae6870430d222879f393
RobinWeiler <r.weiler@student.vu.nl>
2023-12-18 12:29:38 +0100

    Bandpass changes can be reversed

--- callbacks/preprocessing_callbacks.py
+++ callbacks/preprocessing_callbacks.py
@@ -1,6 +1,8 @@
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
+import globals
+
 
 def register_preprocessing_callbacks(app):
     @app.callback(
@@ -8,11 +10,14 @@ def register_preprocessing_callbacks(app):
         [Input("high-pass", "value"), Input("low-pass", "value")],
         State('plot-button', 'n_clicks')
     )
-    def _preprocessing_changed(high_pass, low_pass, plot_button):
-        """Sets n_clicks of hidden-bandpass-changed to 1 if either high-pass, low-pass or bad-channel-interpolation changed.
+    def _bandpass_changed(high_pass, low_pass, plot_button):
+        """Sets n_clicks of hidden-bandpass-changed to 1 if either high-pass or low-pass changed.
         """
         if not plot_button:
             raise PreventUpdate
+
+        if (not high_pass or float(high_pass) == globals.raw.info['highpass']) and (not low_pass or float(low_pass) == globals.raw.info['lowpass']):
+            return 0
 
         # print('Bandpass changed')
         return 1
@@ -22,7 +27,7 @@ def register_preprocessing_callbacks(app):
         Input("confirm-plot-button", "n_clicks"),
         prevent_initial_call=True
     )
-    def _plot_button_pressed(plot_button):
+    def _confirm_plot_button_pressed(plot_button):
         """Sets n_clicks of hidden-bandpass-changed to 0 when confirm-plot-button is pressed.
         """
         # print('Reset bool')
@@ -33,7 +38,7 @@ def register_preprocessing_callbacks(app):
         Input('upload-file', 'filename'),
         prevent_initial_call=True
     )
-    def _plot_button_pressed(selected_file_name):
+    def _reset_plot_button(selected_file_name):
         """Sets n_clicks of plot-button to 0 when new file is loaded.
         """
         return 0
